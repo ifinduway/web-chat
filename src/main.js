63391a1ce39bba1d4197535a5fed01372cc501dc
Evgeniy Levkin <e.levkin@paymo.ru>
2021-04-17 20:57:31 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router'
 import App from './App.vue';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 Vue.config.productionTip = false;
+Vue.use(VueRouter)
 
 new Vue({
   render: (h) => h(App),
